8f4e2dd942e6555556b2a6c41f155a9de28d3583
Abhishek-aks <117778085+Abhishek-aks@users.noreply.github.com>
2023-07-24 04:36:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,38 +1,21 @@
-// let namexx = prompt('Enter a string');
+let namexx = prompt('Enter a string');
 
-// let val;
-// let taskobj={};
-// let count=1;
-// for(let i=0;i<namexx.length;i++){
-//     if(taskobj.hasOwnProperty(namexx[i])){
-//         taskobj[namexx[i]]++;
-//     }else{
-//         taskobj[namexx[i]]=count;
-//     }
-// }
-// console.log(taskobj);
-// for(let i=0;i<namexx.length;i++){
-//     if(taskobj[namexx[i]]===1){
-//         val=namexx[i];        
-//      break;
-//     }
-// }
-// let bar = confirm(val);
-// console.log(namexx, bar);
-
-
-function nonRepeatingCharacter(s){
-	let object = {};
-   for(let char of s)
-	{		object[char] = (object[char] || 0)+1;
-	}
-
-	for(let char of s)
-		{	if(object[char]==1)
-			{	return char;
-			}
-		}
+let val;
+let taskobj={};
+let count=1;
+for(let i=0;i<namexx.length;i++){
+    if(taskobj.hasOwnProperty(namexx[i])){
+        taskobj[namexx[i]]++;
+    }else{
+        taskobj[namexx[i]]=count;
+    }
 }
-const userInput = prompt("Enter a String");
-const result = nonRepeatingCharacter(userInput);
-alert(result);
+console.log(taskobj);
+for(let i=0;i<namexx.length;i++){
+    if(taskobj[namexx[i]]===1){
+        val=namexx[i];        
+     break;
+    }
+}
+let bar = confirm(val);
+console.log(namexx, bar);
